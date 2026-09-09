66949a729c5cf3c7383bc0ce092306aa9b6cd4cb
stickyburn <37672190+stickyburn@users.noreply.github.com>
2026-09-10 04:47:40 +0530

    style: switch dark canvas

--- hypr/theme.lua
+++ hypr/theme.lua
@@ -4,5 +4,5 @@ return {
     active_border = { colors = { "rgb(c79eff)", "rgb(ff91c3)" }, angle = 35 },
     inactive_border = "rgb(2a1d31)",
   },
-  shadow = 0x9914111B,
+  shadow = 0x99221727,
 }
